32e4ed3a783b02356c055e3d6af15798712f650d
Vachounet <vachounet@live.fr>
2021-02-05 14:06:02 +0100

    sm6150_common: initial device tree

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,48 @@
+# Copyright (C) 2021 The OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for grouper hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+# Inherit from dynamic_common
+-include device/motorola/dynamic_common/BoardConfigDynamicCommon.mk
+
+SM6150_PATH := device/motorola/sm6150_common
+
+TARGET_BOOTLOADER_BOARD_NAME := sm6150
+TARGET_BOARD_PLATFORM := sm6150
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno618
+
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 earlycon=msm_geni_serial,0x880000 loop.max_part=7 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET     := 0x01000000
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_SOURCE := kernel/motorola/sm6150
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+TARGET_KERNEL_VERSION := 4.19
+
+TARGET_SPECIFIC_HEADER_PATH := $(SM6150_PATH)/include
+
+TARGET_SYSTEM_PROP += $(SM6150_PATH)/system.prop
+
+PRODUCT_SOONG_NAMESPACES += $(SM6150_PATH)
---
+++ sm6150_common.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for grouper hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+$(call inherit-product, device/motorola/dynamic_common/dynamic_common.mk)
+
+# Boot control
+PRODUCT_PACKAGES += \
+    bootctrl.sm6150.recovery
+
+PRODUCT_AAPT_CONFIG := xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_PACKAGES += \
+    MotoActions
+
+DEVICE_PACKAGE_OVERLAYS += device/motorola/sm6150_common/overlay/device
